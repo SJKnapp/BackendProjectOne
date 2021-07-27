d9c0ee5b25d80edb20ffe97a38d0083d503e50c2
SJKnapp <samuel.j.knapp@outlook.com>
2021-07-27 12:45:19 +0100

    PROJ-3 #comment added place holder test sql for h2 database and put in a test propertie configuration

---
+++ src/test/resources/ToDoList-Schema.sql
@@ -0,0 +1,8 @@
+drop table if exists to_do_list;
+create table to_do_list 
+	(id integer not null auto_increment,
+	description varchar(255), due_date time, 
+	name varchar(255), 
+	priority integer not null, 
+	time_estimate_minutes integer not null, 
+	primary key (id))
