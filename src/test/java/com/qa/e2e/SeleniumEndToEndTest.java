2b5eb72297a50bb7502e44f3c4841e06e02944ee
SJKnapp <samuel.j.knapp@outlook.com>
2021-07-28 17:17:28 +0100

    PROJ-78 #comment added delete test in selenium

--- src/test/java/com/qa/e2e/SeleniumEndToEndTest.java
+++ src/test/java/com/qa/e2e/SeleniumEndToEndTest.java
@@ -1,5 +1,7 @@
 package com.qa.e2e;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -11,7 +13,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -20,7 +21,6 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
 @Sql(scripts = { "classpath:ToDoList-schema.sql",
 		"classpath:ToDoList-data.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 @ActiveProfiles("test")
@@ -39,25 +39,34 @@ public class SeleniumEndToEndTest {
 		driver.manage().window().maximize();
 
 		this.wait = new WebDriverWait(driver, 5);
+
 	}
 
 	@Test
 	void delete() {
-
-	}
-
-	@Test
-	void testers() {
 		driver.get("http://localhost:" + port + "/");
 		WebElement wait = this.wait.until(ExpectedConditions.presenceOfElementLocated(By.id("renderedTasks")));
-		WebElement w = driver.findElement(By.cssSelector("#renderedTasks > div:nth-child(1) > input:nth-child(1)"));
-		// wait.findElements(By.cssSelector("div:nthchild"));
-		w.sendKeys("aaaaa");
 		List<WebElement> pageElements = driver.findElements(By.cssSelector("#renderedTasks > div"));
-		WebElement r = pageElements.get(0).findElement(By.cssSelector("input:nth-child(1)"));
-		r.clear();
-		System.out.println(pageElements);
+		int startSize = pageElements.size();
+		WebElement deleteButton = pageElements.get(0).findElement(By.cssSelector("button:nth-child(7)"));
+		deleteButton.click();
+		pageElements = driver.findElements(By.cssSelector("#renderedTasks > div"));
+		int endSize = pageElements.size();
+		assertEquals(startSize, endSize + 1);
 	}
+
+//	@Test
+//	void testers() {
+//		driver.get("http://localhost:" + port + "/");
+//		WebElement wait = this.wait.until(ExpectedConditions.presenceOfElementLocated(By.id("renderedTasks")));
+//		WebElement w = driver.findElement(By.cssSelector("#renderedTasks > div:nth-child(1) > input:nth-child(1)"));
+//		// wait.findElements(By.cssSelector("div:nthchild"));
+//		w.sendKeys("aaaaa");
+//		List<WebElement> pageElements = driver.findElements(By.cssSelector("#renderedTasks > div"));
+//		WebElement r = pageElements.get(0).findElement(By.cssSelector("input:nth-child(1)"));
+//		r.clear();
+//		System.out.println(pageElements);
+//	}
 
 	@AfterEach
 	void tearDown() {
