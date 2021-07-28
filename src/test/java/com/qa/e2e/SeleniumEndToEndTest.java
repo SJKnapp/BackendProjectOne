1d50ca4d2eb4540232cfa48420bcf38985559b6c
SJKnapp <samuel.j.knapp@outlook.com>
2021-07-28 15:56:21 +0100

    PROJ-78 #comment got selenium to use the test port

--- src/test/java/com/qa/e2e/SeleniumEndToEndTest.java
+++ src/test/java/com/qa/e2e/SeleniumEndToEndTest.java
@@ -1,16 +1,33 @@
 package com.qa.e2e;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Sql(scripts = { "classpath:ToDoList-schema.sql",
+		"classpath:ToDoList-data.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 @ActiveProfiles("test")
 public class SeleniumEndToEndTest {
+
+	@LocalServerPort
+	private int port;
+	private WebDriverWait wait;
 
 	WebDriver driver = new FirefoxDriver();
 
@@ -19,10 +36,20 @@ public class SeleniumEndToEndTest {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		// maximize window
 		driver.manage().window().maximize();
+
+		this.wait = new WebDriverWait(driver, 5);
+	}
+
+	@Test
+	void delete() {
+
 	}
 
 	@Test
 	void testers() {
-		driver.get("http://127.0.0.1/");
+		driver.get("http://localhost:" + port + "/");
+		WebElement wait = this.wait.until(ExpectedConditions.presenceOfElementLocated(By.id("renderedTasks")));
+		List<WebElement> pageElements = driver.findElements(By.id("renderedTasks"));
+		System.out.println(pageElements);
 	}
 }
